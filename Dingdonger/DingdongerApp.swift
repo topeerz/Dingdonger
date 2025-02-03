4c453119f536dd39b1e507db49b3a9a827d4b958
Jan Maluszynski <topeerz@users.noreply.github.com>
2025-02-03 13:01:03 +0100

    Xcode generated project plus gitignore files

---
+++ Dingdonger/DingdongerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DingdongerApp.swift
+//  Dingdonger
+//
+//  Created by topeerz on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct DingdongerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DingdongerTests/DingdongerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DingdongerTests.swift
+//  DingdongerTests
+//
+//  Created by topeerz on 03/02/2025.
+//
+
+import Testing
+@testable import Dingdonger
+
+struct DingdongerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
